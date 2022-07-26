4d251553ab93fb7704afc61bc1eb86c0d0074fab
AzureAD\ジョブトレIT･WEB渋谷　菅原康弘 <yasuhiro.sugawara_04@atgp-jobtra.jp>
2022-07-26 13:55:48 +0900

    vue.jsのプロジェクトをあげました。

---
+++ vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+<div></div>
+</template>
+
+<script>
+import axios from "axios"
+export default{
+  mounted(){
+    axios.get('https://sample.novalumoapis.com/api/users')
+  }
+}
+</script>
