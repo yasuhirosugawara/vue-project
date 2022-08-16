1a82d829935d6b6ef80cb0156069505b31385a2d
AzureAD\ジョブトレIT･WEB渋谷　菅原康弘 <yasuhiro.sugawara_04@atgp-jobtra.jp>
2022-08-16 14:01:00 +0900

    本日の面談でaxiosでのpostの結果が反映できるように修正した内容です。

--- vue-project/src/views/Hoge2View.vue
+++ vue-project/src/views/Hoge2View.vue
@@ -4,31 +4,55 @@ import axios from 'axios'
 
 <template>
   <main>
-    <input type="text" :value="name" placeholder="name" v-on:input="console.log(this.name)"><br>
-    <input type="text" :value="email" placeholder="email"><br>
+    <input type="text" v-model="name" placeholder="name" v-on:input="console.log(this.name)"><br>
+    <input type="text" v-model="email" placeholder="email"><br>
     <button @click="send()">送信</button>
   </main>
 </template>
 
 <script>
-export default {
-  methods: {
-    send() {
-      axios.post('https://sample.novalumoapis.com/api/users', {
-        name: this.name,
-        email: this.email
-      }).then((res) => {
-        console.log('Success')
-      }).catch(() => {
-        console.error('Error')
-      })
-    }
-  },
-  data() {
-    return {
-      name: "",
-      email: ""
-    }
-  }
-}
+  export default {
+    data() {
+      return {
+        name: "",
+        email: "",
+      };
+    },
+    methods: {
+      send: function () {
+        axios
+          .post('https://sample.novalumoapis.com/api/users', {
+            name: this.name,
+            email: this.email,
+          })
+          .then((res) => {
+            console.log(res);
+            this.posts = res.data.posts;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+    },
+  };
+// export default {
+//     data() {
+//     return {
+//       name: "",
+//       email: ""
+//     }
+//   },
+//   methods: {
+//     send() {
+//       axios.post('https://sample.novalumoapis.com/api/users', {
+//         name: this.name,
+//         email: this.email
+//       }).then((res) => {
+//         console.log('Success')
+//       }).catch(() => {
+//         console.error('Error')
+//       })
+//     }
+//   }
+// }
 </script>
